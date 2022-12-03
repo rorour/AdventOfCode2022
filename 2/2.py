c83a83946a20f1028ec3b65321190273db84d9b8
rorour <ravenmarieorourke@gmail.com>
2022-12-03 13:27:50 -0700

    script for initializing new day

---
+++ 2/2.py
@@ -0,0 +1,14 @@
+from main import get_input_data, results
+
+
+def first():
+    pass
+
+
+def second():
+    pass
+
+
+data = get_input_data()
+results(first, second)
+
---
+++ new.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+day=$1
+mkdir $day
+touch $day/input
+touch $day/test_input
+cat << EOF > $day/$day.py
+from main import get_input_data, results
+
+
+def first():
+    pass
+
+
+def second():
+    pass
+
+
+data = get_input_data()
+results(first, second)
+
+EOF
+
