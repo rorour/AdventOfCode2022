a90e87c0348c3fdbdae0181714f6485939bad6db
rorour <ravenmarieorourke@gmail.com>
2022-12-04 12:45:47 -0700

    use clearer var names

--- 1/1.py
+++ 1/1.py
@@ -3,14 +3,14 @@ from main import get_input_data, results
 
 def _get_calorie_totals():
     calorie_totals = []
-    x = 0
-    for l in data:
-        if l is None:
-            calorie_totals.append(x)
-            x = 0
+    single_elf_total = 0
+    for line in data:
+        if line is None:
+            calorie_totals.append(single_elf_total)
+            single_elf_total = 0
         else:
-            x += l
-    calorie_totals.append(x)
+            single_elf_total += line
+    calorie_totals.append(single_elf_total)
     return calorie_totals
 
 
